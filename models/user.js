266127dbc3caede2595ede2ce326c952458fa3da
Taioh Kubota <kubotataioh@gmail.com>
2022-05-25 16:51:40 -0400

    created database and starter files

---
+++ models/user.js
@@ -0,0 +1,76 @@
+/** User class for message.ly */
+
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const ExpressError = require("../expressError")
+const {BCRYPT_WORK_FACTOR} = require("../config");
+/** User of the site. */
+
+class User {
+
+  /** register new user -- returns
+   *    {username, password, first_name, last_name, phone}
+   */
+
+  static async register({username, password, first_name, last_name, phone}) {
+    const hashedPass = await bcrypt.hash(password, BCRYPT_WORK_FACTOR)
+    const result = await db.query(
+      `INSERT INTO users
+      (username, password, first_name, last_name, phone, join_at, last_login_at)
+      VALUES ($1,$2,$3,$4,$5,current_timestamp,current_timestamp)
+      RETURNING username, password, first_name, last_name, phone`,
+      [username, hashedPass, first_name, last_name, phone]
+    )
+
+    return result.rows[0];
+   }
+
+  /** Authenticate: is this username/password valid? Returns boolean. */
+
+  static async authenticate(username, password) {
+
+   }
+
+  /** Update last_login_at for user */
+
+  static async updateLoginTimestamp(username) { }
+
+  /** All: basic info on all users:
+   * [{username, first_name, last_name, phone}, ...] */
+
+  static async all() { }
+
+  /** Get: get user by username
+   *
+   * returns {username,
+   *          first_name,
+   *          last_name,
+   *          phone,
+   *          join_at,
+   *          last_login_at } */
+
+  static async get(username) { }
+
+  /** Return messages from this user.
+   *
+   * [{id, to_user, body, sent_at, read_at}]
+   *
+   * where to_user is
+   *   {username, first_name, last_name, phone}
+   */
+
+  static async messagesFrom(username) { }
+
+  /** Return messages to this user.
+   *
+   * [{id, from_user, body, sent_at, read_at}]
+   *
+   * where from_user is
+   *   {username, first_name, last_name, phone}
+   */
+
+  static async messagesTo(username) { }
+}
+
+
+module.exports = User;
